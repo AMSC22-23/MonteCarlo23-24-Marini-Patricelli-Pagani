98d69e61d33f4bb32c71dca9ae1dd4daa71b925b
Patt126 <145294744+Patt126@users.noreply.github.com>
2024-01-10 15:27:16 +0100

    Add files via upload

--- Serial/serailRefactored/SquareLattice.h
+++ Serial/serailRefactored/SquareLattice.h
@@ -1,28 +1,48 @@
-// SquareLattice.h
-
 #ifndef SQUARELATTICE_H
 #define SQUARELATTICE_H
 
 #include "AbstractLattice.h"
 #include <iostream>
-#include <array>
+#include <memory>
+#include <vector>
 
-template <int N>
-class SquareLattice : public AbstractLattice<N> {
+class SquareLattice : public AbstractLattice {
 public:
     SquareLattice(float interactionStrength, int latticeSize);
 
     void printLattice() const override;
     float evaluateEnergy() const override;
     void initialize() override;
-    const std::array<int, N>& getLattice() const;
+    std::vector<int>& getLattice();
+    float getInteractionEnergy() const override {
+        return J;
+    }
+    float getMagnetization() const {
+        return M;
+    }
+    void incrementMagnetization(int Madd) {
+        M += Madd;
+    }
+    void incrementEnergy(int Eadd) {
+        E += Eadd;
+    }
+    void restoreRandomLattice() {
+        lattice->assign(randomLattice->begin(), randomLattice->end());
+        M = M_rand;
+        E = E_rand;
+    }
 
 private:
     const int L;
     const int J;
-    std::array<int, N> lattice;
+    int N; 
+    int M;
+    float E;
+    int M_rand;
+    float E_rand;
+    std::unique_ptr<std::vector<int>> lattice;
+    std::unique_ptr<std::vector<int>> randomLattice;
 };
 
-#include "SquareLattice.tpp"  // Include template implementation
 
 #endif // SQUARELATTICE_H
