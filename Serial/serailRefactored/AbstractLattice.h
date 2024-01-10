d1b33c75f9c8d6c446ec27aacedc9633543a2c5c
Patt126 <145294744+Patt126@users.noreply.github.com>
2024-01-10 15:04:00 +0100

    Add files via upload

--- Serial/serailRefactored/AbstractLattice.h
+++ Serial/serailRefactored/AbstractLattice.h
@@ -3,16 +3,15 @@
 #ifndef ABSTRACTLATTICE_H
 #define ABSTRACTLATTICE_H
 
-template <int N>
 class AbstractLattice {
 public:
-    virtual AbstractLattice() = default;
     virtual void initialize() = 0;
     virtual float evaluateEnergy() const = 0;
     virtual void printLattice() const = 0;
+    virtual float getInteractionEnergy() const = 0; 
     virtual ~AbstractLattice() = default;
-    virtual double getInteractionEnergy() = 0; 
-    virtual void storeResultsToFile() const = 0;
+    
+   
 };
 
 #endif // ABSTRACTLATTIC
--- Serial/serailRefactored/AbstractMonteCarloSimulation.h
+++ Serial/serailRefactored/AbstractMonteCarloSimulation.h
@@ -3,17 +3,15 @@
 
 #include <array>
 
-template <std::size_t N>
 class AbstractMonteCarloSimulation {
 public:
     virtual void simulatePhaseTransition() = 0;
 protected:
     virtual void createRandVector() = 0;
-    virtual void flip(std::array<int, N>& lattice, std::array<float, 2>& prob, int site, int& M, int& E) = 0;
-    virtual void simulateStep(std::array<float, 2> prob, std::array<int, N>& lattice, int& M, int& E) = 0;
-    virtual float mexact(float T){
-        return std::pow((1.0 - std::pow(std::sinh(2 * J / T), -4)), 1.0 / 8.0);
-    }
+    virtual void flip(std::vector<int>& lattice, std::array<float, 2>& prob, int site, int& M, int& E) = 0;
+    virtual void simulateStep(std::array<float, 2> prob, std::vector<int>& lattice, int& M, int& E) = 0;
+    virtual void storeResultsToFile() const = 0;
+    virtual float mexact(float T) const = 0;
 
     
 };
